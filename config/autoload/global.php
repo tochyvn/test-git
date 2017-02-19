8c80e3ca762d7402f6bf4c8dc71e03d065829926
tochyvn <tochlionimy@gmail.com>
2017-02-19 08:45:42 +0100

    Mise en ligne de mon projet

---
+++ config/autoload/global.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+
+return [
+    'db' => array (
+				'driver' => 'Pdo',
+				'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=zf-tutorial',
+				'username' => 'root',
+				'password' => ''
+		),
+		'service_manager' => array (
+				'factories' => array (
+						'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory' 
+				) 
+		)
+];
---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Album\Model\AlbumTable;
+
+class AlbumController extends AbstractActionController
+{
+    private $table;
+
+    public function __construct(AlbumTable $table)
+    {
+        $this->table = $table;
+    }
+
+    public function indexAction()
+    {
+        return new ViewModel([
+            'table' => $this->table
+        ]);
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
